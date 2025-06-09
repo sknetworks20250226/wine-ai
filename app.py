7605db91561b5e3317089d70cc8eed0628eeb655
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-09 17:09:14 +0900

    modify

--- app.py
+++ app.py
@@ -21,19 +21,23 @@ with col1:
                 # 멀티모달 모델을 이용해서 사진의 요리의 특성을 분석
                 # 출력                                
                 st.markdown('### 요리의 맛과 향 분석 결과')                
-                dish_flavor = 
+                image_bytes = uploaded_image.read()
+                dish_flavor = describe_dish_flavor(image_bytes=image_bytes,query='이 요리의 맛과 향과 같은 특징을 한글로 분석해 주세요')
                 st.info( dish_flavor )
             with st.spinner('2단계 : 요리에 어울리는 와인 리뷰를 검색하는 중....'):
                 # 요리의 특성정보로 와인을 추천ai 동작                
                 time.sleep(3)
                 st.markdown('### 와인 리뷰 검색 결과')
-                wine_search_result = 
+                wine_search_result = search_wine(dish_flavor)
                 st.text(wine_search_result['wine_reviews'])
             with st.spinner('3단계 : AI 소믈리에가 와인 페이링에 대한 추천글을 생성하는 중...'):
                 # LLM을 이용해서 추천글 생성                
                 time.sleep(3)
                 st.markdown('### AI 소믈리에 와인 페어링 추천')
-                wine_recommandation = 
+                wine_recommandation = recommand_wine({
+                    'dish_flavor' : dish_flavor,
+                    'wine_reviews' : wine_search_result['wine_reviews']
+                })
                 st.info( wine_recommandation)
             st.success('추천이 완료되었습니다.')
 # 유사도를 높이도록 -- 

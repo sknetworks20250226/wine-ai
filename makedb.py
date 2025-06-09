fab20579116e9bf889462497842094c0025e2e41
sknetworks20250226 <sknetworks.20250226@gmail.com>
2025-06-09 15:34:42 +0900

    업데이트.

--- makedb.py
+++ makedb.py
@@ -15,3 +15,26 @@ pc.create_index(
 )
 wine_index = pc.Index(os.environ['PINECONE_INDEX_NAME'])
 print(wine_index.describe_index_stats())
+
+# csv 파일 읽어서 벡터 db 만들기
+from langchain_community.document_loaders import CSVLoader
+loader = CSVLoader('winemag-data-130k-v2.csv', encoding='utf-8')
+docs = loader.load()
+# 임베딩 모델
+from langchain_openai import OpenAIEmbeddings
+embedding = OpenAIEmbeddings(model=os.environ['OPENAI_EMBEDDING_MODEL'])
+
+# 벡터화 해서 DB에 저장
+from langchain_pinecone import PineconeVectorStore
+BATCH_SIZE = 300
+for i in range(0,len(docs), BATCH_SIZE):
+    batch = docs[i : i+BATCH_SIZE]
+    try:
+        PineconeVectorStore.from_documents(
+            documents=batch,
+            index_name = os.environ['PINECONE_INDEX_NAME'],
+            embedding=embedding
+        )
+        print(f'{i} ~ {i+len(batch)-1} documents indexed')
+    except Exception as e:
+        print(f'{i} ~ {i+len(batch)-1}  error : {e}')
